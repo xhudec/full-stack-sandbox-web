2d709297893b24409445310dcf314c49bb08ba91
Lukas Hudec <hudec.luke@gmail.com>
2019-10-20 15:07:20 +0200

    chore: jest setup

---
+++ components/button/__tests__/index.spec.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { RenderResult, fireEvent } from '@testing-library/react'
+
+import Button, { IButtonProps } from '..'
+import renderWithTheme from '@test/helpers/renderers/renderWithTheme'
+
+function renderButton(propOverrides: Partial<IButtonProps>): RenderResult {
+  const testUtils = renderWithTheme(
+    <Button onClick={() => {}} {...propOverrides}>
+      {propOverrides.children || ''}
+    </Button>
+  )
+
+  return testUtils
+}
+
+describe('<Button />', () => {
+  it('should be defined', () => {
+    expect(typeof Button).toBe('function')
+  })
+
+  it('should render a button with text', () => {
+    const { getByText } = renderButton({ children: 'Click' })
+
+    getByText('Click')
+  })
+
+  it('should trigger an onClick handler when clicked', () => {
+    const onClickSpy = jest.fn()
+    const { getByText } = renderButton({ children: 'test', onClick: onClickSpy })
+
+    const buttonElement = getByText('test')
+    fireEvent.click(buttonElement)
+
+    expect(onClickSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should not trigger an onClick handler when clicked and disabled', () => {
+    const onClickSpy = jest.fn()
+    const { getByText } = renderButton({ children: 'test', onClick: onClickSpy, isDisabled: true })
+
+    const buttonElement = getByText('test')
+    fireEvent.click(buttonElement)
+
+    expect(onClickSpy).toHaveBeenCalledTimes(0)
+  })
+})
---
+++ test/helpers/renderers/renderWithTheme.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import { render, RenderResult } from '@testing-library/react'
+import defaultTheme, { ITheme } from '@theme'
+
+export default function renderWithTheme(
+  ui: JSX.Element,
+  theme: ITheme = defaultTheme
+): RenderResult {
+  return render(<ThemeProvider theme={theme}>{ui}</ThemeProvider>)
+}
