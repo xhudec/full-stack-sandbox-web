efd8729ce17721b281c5f5e8072ffb147eb7041a
Lukas Hudec <hudec.luke@gmail.com>
2019-10-20 15:05:34 +0200

    feat: add theme

---
+++ theme/index.ts
@@ -0,0 +1,15 @@
+// TODO: setup theme
+const theme = {
+  colors: {
+    primary: '#00F',
+    secondary: '',
+    success: '',
+    warning: '',
+    error: '',
+    disabled: '#343434',
+  },
+}
+
+export type ITheme = typeof theme
+
+export default theme
