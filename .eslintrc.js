e13d2412cb6fdb6fd13f99169cc2dba3997a1d86
Lukas Hudec <hudec.luke@gmail.com>
2019-10-20 15:03:13 +0200

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    '@strv/eslint-config-typescript',
+    '@strv/eslint-config-typescript/optional',
+    '@strv/eslint-config-typescript/style',
+
+    '@strv/eslint-config-react',
+    '@strv/eslint-config-react/optional',
+    '@strv/eslint-config-react/style',
+
+    '@strv/eslint-config-node/v10',
+    '@strv/eslint-config-node/optional',
+    '@strv/eslint-config-node/style',
+  ],
+  plugins: ['@typescript-eslint', 'import', 'jest'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+    /**
+     * ! FIXME: because ESLint can't detect newly created modules for some reason
+     * @see https://github.com/typescript-eslint/typescript-eslint/issues/864#issuecomment-538167956
+     */
+    createDefaultProgram: true,
+  },
+  env: {
+    'jest/globals': true,
+  },
+  rules: {
+    // conflicts with personal preference or guidelines
+    'padding-line-between-statements': 'off',
+    'arrow-body-style': 'off',
+    'import/order': 'off',
+    'import/group-exports': 'off',
+    '@typescript-eslint/member-ordering': 'off',
+
+    // prettier is responsible for all code formatting
+    'comma-dangle': 'off',
+    'function-paren-newline': 'off',
+    'no-extra-parens': 'off',
+
+    // typescript...
+    'react/prop-types': 'off',
+
+    // ! FIXME: strange behavior
+    'import/no-unused-modules': 'off',
+  },
+}
