382c1581d063d8940457e7e257faa60d1197931d
Lukas Hudec <hudec.luke@gmail.com>
2020-01-23 20:35:23 +0100

    chore(github): setup github actions (#6)
    * ci(github): add static-tests action
    
    * ci(actions): add app-tests job

--- components/context-controller/__server_tests__/index.spec.ts
+++ components/context-controller/__server_tests__/index.spec.ts
@@ -1,5 +1,11 @@
+import ContextController from '..'
+
 describe('<ContextController />', () => {
   it('fancy test', () => {
     expect(true).toBe(true)
+  })
+
+  it('should be defined', () => {
+    expect(typeof ContextController).toBe('function')
   })
 })
